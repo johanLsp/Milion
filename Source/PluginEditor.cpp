9dd0c4f0344c2c969d7ea47657ba60dcc112ecc6
johanLsp <johan.lasperas@gmail.com>
2017-12-10 20:00:31 +0100

    Add midi keyboard component

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -3,7 +3,10 @@
 
 //==============================================================================
 MilionAudioProcessorEditor::MilionAudioProcessorEditor(MilionAudioProcessor& p)
-    : AudioProcessorEditor(&p), processor(p) {
+    : AudioProcessorEditor(&p),
+      processor(p),
+      m_midiKeyboard(m_keyboardState, MidiKeyboardComponent::horizontalKeyboard)
+      {
     m_frequencyKnob.setSliderStyle(Slider::Rotary);
     m_frequencyKnob.setRange(100, 10000, 1);
     m_frequencyKnob.setSkewFactor(0.5);
@@ -11,8 +14,11 @@ MilionAudioProcessorEditor::MilionAudioProcessorEditor(MilionAudioProcessor& p)
     m_frequencyKnob.setTextValueSuffix(" Hz");
     m_frequencyKnob.addListener(this);
 
-    addAndMakeVisible(&m_frequencyKnob);
+    m_keyboardState.addListener (this);
 
+
+    addAndMakeVisible(&m_frequencyKnob);
+    addAndMakeVisible(&m_midiKeyboard);
     setSize(400, 300);
 }
 
@@ -34,5 +40,22 @@ void MilionAudioProcessorEditor::paint(Graphics& g) {
 
 void MilionAudioProcessorEditor::resized() {
     Rectangle<int> area = getLocalBounds();
+    m_midiKeyboard.setBounds(area.removeFromBottom(50));
     m_frequencyKnob.setBounds(area.reduced(50));
+}
+
+
+void MilionAudioProcessorEditor::handleNoteOn (MidiKeyboardState*, int midiChannel, int midiNoteNumber, float velocity)
+{
+    //MidiMessage m (MidiMessage::noteOn (midiChannel, midiNoteNumber, velocity));
+    //m.setTimeStamp (Time::getMillisecondCounterHiRes() * 0.001);
+    //sendToOutputs (m);
+}
+
+//==============================================================================
+void MilionAudioProcessorEditor::handleNoteOff (MidiKeyboardState*, int midiChannel, int midiNoteNumber, float velocity)
+{
+    //MidiMessage m (MidiMessage::noteOff (midiChannel, midiNoteNumber, velocity));
+    //m.setTimeStamp (Time::getMillisecondCounterHiRes() * 0.001);
+    //sendToOutputs (m);
 }
--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -5,20 +5,26 @@
 #include "PluginProcessor.h"
 
 class MilionAudioProcessorEditor  : public AudioProcessorEditor,
-                                      private Slider::Listener {
+                                    private Slider::Listener,
+                                    private MidiKeyboardStateListener {
  public:
     explicit MilionAudioProcessorEditor(MilionAudioProcessor&);
     ~MilionAudioProcessorEditor();
 
     void paint(Graphics&) override;
     void resized() override;
+    void handleNoteOn(MidiKeyboardState*, int midiChannel, int midiNoteNumber, float velocity) override;
+    void handleNoteOff(MidiKeyboardState*, int midiChannel, int midiNoteNumber, float velocity) override;
 
  private:
     void sliderValueChanged(Slider* slider) override;
 
+
  private:
     MilionAudioProcessor& processor;
     Slider m_frequencyKnob;
+    MidiKeyboardState m_keyboardState;
+    MidiKeyboardComponent m_midiKeyboard;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(MilionAudioProcessorEditor)
 };
