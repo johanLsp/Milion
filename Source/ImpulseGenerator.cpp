03788d177d18c1d5d58479d39814f06c5e800981
johanLsp <johan.lasperas@gmail.com>
2017-12-27 22:30:17 +0100

    Inherit ImpulseGenerator from AudioProcessor

--- Source/ImpulseGenerator.cpp
+++ Source/ImpulseGenerator.cpp
@@ -1,15 +1,68 @@
 #include "ImpulseGenerator.h"
 
-ImpulseGenerator::ImpulseGenerator(int sampleRate)
-: m_sampleRate(sampleRate), m_currentTick(0) {
+ImpulseGenerator::ImpulseGenerator()
+: m_currentTick(0) {
     m_frequency = 1000;
+}
+
+
+
+const String ImpulseGenerator::getName() const {
+    return "Impulse Generator Processor";
+}
+
+void ImpulseGenerator::releaseResources() {
+}
+
+double ImpulseGenerator::getTailLengthSeconds() const {
+    return 0.0;
+}
+
+bool ImpulseGenerator::acceptsMidi() const {
+    return false;
+}
+
+bool ImpulseGenerator::producesMidi() const {
+    return false;
+}
+
+AudioProcessorEditor* ImpulseGenerator::createEditor() {
+
+}
+
+bool ImpulseGenerator::hasEditor() const {
+    return false;
+}
+
+int ImpulseGenerator::getNumPrograms() {
+    return 1;
+}
+
+int ImpulseGenerator::getCurrentProgram() {
+    return 0;
+}
+
+void ImpulseGenerator::setCurrentProgram(int index) {
+}
+
+const String ImpulseGenerator::getProgramName(int index) {
+    return {};
+}
+
+void ImpulseGenerator::changeProgramName(int index, const String& newName) {
+}
+
+void ImpulseGenerator::getStateInformation(MemoryBlock& destData) {
+}
+
+void ImpulseGenerator::setStateInformation(const void* data, int sizeInBytes) {
 }
 
 void ImpulseGenerator::setFrequency(int frequency) {
     m_frequency = frequency;
 }
 void ImpulseGenerator::processBlock(AudioSampleBuffer& buffer) {
-    int period = m_sampleRate / m_frequency;
+    int period = getSampleRate() / m_frequency;
     float* channelData = buffer.getWritePointer(0);
 
     for (int i = 0; i < buffer.getNumSamples(); i++) {
--- Source/ImpulseGenerator.h
+++ Source/ImpulseGenerator.h
@@ -4,16 +4,34 @@
 #include "../JuceLibraryCode/JuceHeader.h"
 
 
-class ImpulseGenerator {
+class ImpulseGenerator : public AudioProcessor {
  public:
-    ImpulseGenerator(int sampleRate);
+    ImpulseGenerator();
     ~ImpulseGenerator();
+
+    void prepareToPlay(double sampleRate, int samplesPerBlock) override;
+    void processBlock(AudioSampleBuffer &buffer, MidiBuffer &midiMessages) override;
+    const String getName() const override;
+    void releaseResources() override;
+    double getTailLengthSeconds() const override;
+    bool acceptsMidi() const override;
+    bool producesMidi() const override;
+    AudioProcessorEditor* createEditor() override;
+    bool hasEditor() const override;
+
+    int getNumPrograms() override;
+    int getCurrentProgram() override;
+    void setCurrentProgram(int index) override;
+    const String getProgramName(int index) override;
+    void changeProgramName(int index, const String& newName) override;
+
+    void getStateInformation(MemoryBlock& destData) override;
+    void setStateInformation(const void* data, int sizeInBytes) override;
 
     void processBlock(AudioSampleBuffer& buffer);
     void setFrequency(int frequency);
 
  private:
-    int m_sampleRate;
     int m_frequency;
     uint64_t m_currentTick;
 };
