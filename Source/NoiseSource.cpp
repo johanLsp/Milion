d2a1d5576bc40e8d3d730e53be0fa039cd4c9fdc
johanLsp <johan.lasperas@gmail.com>
2017-12-28 18:49:14 +0100

    Add noise source processor

---
+++ Source/NoiseSource.cpp
@@ -0,0 +1,116 @@
+#include "NoiseSource.h"
+
+NoiseSource::NoiseSource() {
+}
+
+NoiseSource::~NoiseSource() {
+}
+
+const String NoiseSource::getName() const {
+    return "Voicing Source Processor";
+}
+
+void NoiseSource::releaseResources() {
+}
+
+double NoiseSource::getTailLengthSeconds() const {
+    return 0.0;
+}
+
+bool NoiseSource::acceptsMidi() const {
+    return false;
+}
+
+bool NoiseSource::producesMidi() const {
+    return false;
+}
+
+AudioProcessorEditor* NoiseSource::createEditor() {
+}
+
+bool NoiseSource::hasEditor() const {
+    return false;
+}
+
+int NoiseSource::getNumPrograms() {
+    return 1;
+}
+
+int NoiseSource::getCurrentProgram() {
+    return 0;
+}
+
+void NoiseSource::setCurrentProgram(int index) {
+}
+
+const String NoiseSource::getProgramName(int index) {
+    return {};
+}
+
+void NoiseSource::changeProgramName(int index, const String& newName) {
+}
+
+void NoiseSource::getStateInformation(MemoryBlock& destData) {
+}
+
+void NoiseSource::setStateInformation(const void* data, int sizeInBytes) {
+}
+
+void NoiseSource::setFrequency(int frequency) {
+    m_randomGenerator.setFrequency(frequency);
+}
+
+
+void NoiseSource::prepareToPlay(double sampleRate, int samplesPerBlock) {
+    m_LPF.setCenterFrequency(0);
+    m_LPF.setBandwidth(100);
+
+    m_graph.setPlayConfigDetails(
+        getTotalNumInputChannels(),
+        getTotalNumOutputChannels(),
+        sampleRate,
+        samplesPerBlock);
+    m_graph.setProcessingPrecision(AudioProcessor::singlePrecision);
+    m_graph.prepareToPlay(sampleRate, samplesPerBlock);
+
+    AudioProcessorGraph::AudioGraphIOProcessor* input =
+        new AudioProcessorGraph::AudioGraphIOProcessor(
+            AudioProcessorGraph::AudioGraphIOProcessor::audioInputNode);
+
+    AudioProcessorGraph::AudioGraphIOProcessor* output =
+        new AudioProcessorGraph::AudioGraphIOProcessor(
+            AudioProcessorGraph::AudioGraphIOProcessor::audioOutputNode);
+
+    m_randomGenerator.setPlayConfigDetails(
+        getTotalNumInputChannels(),
+        getTotalNumOutputChannels(),
+        sampleRate,
+        samplesPerBlock);
+
+    m_LPF.setPlayConfigDetails(
+        getTotalNumInputChannels(),
+        getTotalNumOutputChannels(),
+        sampleRate,
+        samplesPerBlock);
+
+
+    m_graph.addNode(input, 1);
+    m_graph.addNode(output, 2);
+    m_graph.addNode(&m_randomGenerator, 3);
+    m_graph.addNode(&m_LPF, 4);
+
+    // Input -> RandomGenerator
+    m_graph.addConnection(1, 0, 3, 0);
+    m_graph.addConnection(1, 1, 3, 1);
+    // ImpulseGenerator -> LPF
+    m_graph.addConnection(3, 0, 4, 0);
+    m_graph.addConnection(3, 1, 4, 1);
+    // LPF -> Output
+    m_graph.addConnection(4, 0, 2, 0);
+    m_graph.addConnection(4, 1, 2, 1);
+}
+
+
+void NoiseSource::processBlock(AudioSampleBuffer &buffer, MidiBuffer &midiMessages) {
+    m_graph.processBlock(buffer, midiMessages);
+}
---
+++ Source/NoiseSource.h
@@ -0,0 +1,40 @@
+#ifndef MILION_MILIONNOISESOURCE_H_
+#define MILION_MILIONNOISESOURCE_H_
+
+#include "RandomGenerator.h"
+#include "DigitalResonator.h"
+#include "../JuceLibraryCode/JuceHeader.h"
+
+class NoiseSource : public AudioProcessor {
+ public:
+    NoiseSource();
+    ~NoiseSource();
+    void prepareToPlay(double sampleRate, int samplesPerBlock) override;
+    void processBlock(AudioSampleBuffer &buffer, MidiBuffer &midiMessages) override;
+    const String getName() const override;
+    void releaseResources() override;
+    double getTailLengthSeconds() const override;
+    bool acceptsMidi() const override;
+    bool producesMidi() const override;
+    AudioProcessorEditor* createEditor() override;
+    bool hasEditor() const override;
+
+    int getNumPrograms() override;
+    int getCurrentProgram() override;
+    void setCurrentProgram(int index) override;
+    const String getProgramName(int index) override;
+    void changeProgramName(int index, const String& newName) override;
+
+    void getStateInformation(MemoryBlock& destData) override;
+    void setStateInformation(const void* data, int sizeInBytes) override;
+
+    void setFrequency(int frequency);
+
+ private:
+    AudioProcessorGraph m_graph;
+
+    RandomGenerator m_randomGenerator;
+    DigitalResonator m_LPF;
+};
+
+#endif  // MILION_MILIONNOISESOURCE_H_
